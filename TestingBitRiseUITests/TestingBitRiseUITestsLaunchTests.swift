d0f395f686675550682df976e6e606d311d85da6
yacob <yacob.kazal@gmail.com>
2021-11-09 16:19:26 +1100

    Initial Commit

---
+++ TestingBitRiseUITests/TestingBitRiseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestingBitRiseUITestsLaunchTests.swift
+//  TestingBitRiseUITests
+//
+//  Created by yacob kazal on 9/11/21.
+//
+
+import XCTest
+
+class TestingBitRiseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
